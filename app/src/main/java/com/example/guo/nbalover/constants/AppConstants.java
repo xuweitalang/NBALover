395cbcb7cbe542375f7e878c38d058ef37799c4e
xuweitalang <syxyxuwei@163.com>
2017-04-04 22:00:10 +0800

    常量类

---
+++ app/src/main/java/com/example/guo/nbalover/constants/AppConstants.java
@@ -0,0 +1,14 @@
+package com.example.guo.nbalover.constants;
+
+/**
+ * Created by guo on 2017/4/4.
+ */
+
+public class AppConstants {
+    public final static String NBA_MobAPI_Key = "d76224097452875d4a339900db1d4f30";
+
+    public static final String baseURL = "http://v.juhe.cn/nba/";
+    public static final String allTeamInfo = "all_team_info.php"; //所有球队信息
+
+    public static final String NBA_Parameter_Key = "key"; //MobAPI 开发者Key
+}
